d019b61a102764f977c481cff0177a00a8f29d32
Giovana-Manuquian <103048833+Giovana-Manuquian@users.noreply.github.com>
2022-10-24 19:22:35 -0300

    Add files via upload
    Teste front-end atualizado com as informações do site que cada um criou na matéria de front-end.

--- TesteCursoSenai.java
+++ TesteCursoSenai.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
@@ -26,17 +27,37 @@ public class TesteCursoSenai {
 	//teste
 	@Test
 	public void TesteNavegabilidade () {
-		driver.get("https://informatica.sp.senai.br/");
-		driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("Excel");
-		//driver.findElement(By.id("Busca1_btnBusca")).click();
-		driver.findElement(By.id("Busca1_btnBusca")).sendKeys(Keys.ENTER);
+		
+		//uma variável para uma ação na internet
+		WebElement menuServicos = driver.findElement(By.xpath("/html/body/app-root/app-home/header/div/nav/div/a"));
+		WebElement campoEmail = driver.findElement(By.id("email"));
+		
+		try {
+		driver.get("https://localhost:4200/");
+		Thread.sleep(3000);
+		menuServicos.click();
+		Thread.sleep(2000);
+		campoEmail.clear();
+		Thread.sleep(2000);
+		campoEmail.sendKeys("giovana@email.com");
+		Thread.sleep(2000);
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("teste123");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id=\"login-container\"]/form/input[3]")).sendKeys(Keys.ENTER);
+		Thread.sleep(3000);
+		
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
 	
 	
 	//finalização
 	//@After
-	public void Fim (){
+	public void Fim () {
 		driver.quit();
 	}
-		
 }
+
